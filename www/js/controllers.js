a125c255c5dca181a9502ac1f3e185a9dae2ef2d
Nhat Nguyen Developer <nhatndm1193@gmail.com>
2017-01-15 00:13:51 +0700

    update code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,20 @@
 angular.module('app.controllers', [])
   
-.controller('orderListCtrl', function ($scope, $stateParams, BgTrackingService) {    
+.controller('orderListCtrl', function ($scope,$http, $stateParams, $state, API_ENDPOINT, AuthService,$ionicLoading, BgTrackingService) {
+    var getListOrder = function(){
+        $http.get(API_ENDPOINT.url + '/api/orders/findAllOrder/').success(function(response){
+            $ionicLoading.show({
+                template: '<p>Loading...</p><ion-spinner></ion-spinner>',
+            });
+            if(response.success){
+                $ionicLoading.hide();
+                $scope.listOrder = response.data;
+            }    
+         });      
+    }   
+
+    getListOrder();
+
     $scope.startTracking = function(){        
         alert('Start tracking your location');
         BgTrackingService.start();
@@ -17,15 +31,74 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('profileCtrl',function ($scope, $stateParams,$state, AuthService) {
+.controller('profileCtrl',function ($scope,$http, $stateParams, $state, API_ENDPOINT, AuthService,$ionicLoading) {
+    $scope.isEdit = true;
+    $scope.labelButton = "Edit Profile";
+    var getinfo = function(){
+        if(AuthService.isAuthenticated()){
+                $http.get(API_ENDPOINT.url + '/api/users/findone/' + AuthService.tokensave()).success(function(response){
+                if(response.success){
+                    $scope.user = response.data;
+                }
+            });
+        }
+    };
+
+    getinfo();
 
     $scope.logout = function(){
         AuthService.logout();
         $state.go('login');
     };
+
+    $scope.enableEdit = function(){
+        $scope.isEdit = false;   
+    }
+    $scope.cancelEdit = function(){
+       $scope.isEdit = true; 
+    }
+    $scope.updateUser = function(id) {
+        $http.put(API_ENDPOINT.url + '/api/users/update/' + id, $scope.user).success(function(response){
+             $ionicLoading.show({
+                template: '<p>Loading...</p><ion-spinner></ion-spinner>',
+            });
+            if(response.success){
+                $ionicLoading.hide();
+                $scope.isEdit = true;
+            }    
+         });      
+    };
 })
       
-.controller('order1Ctrl', function ($scope, $stateParams) {    
+.controller('order1Ctrl', function ($scope,$http, $stateParams, $state, API_ENDPOINT, AuthService,$ionicLoading) {   
+    var getInfor = function(){
+        $http.get(API_ENDPOINT.url + '/api/orders/findOrder/' + $stateParams.idorder).success(function(response){
+            $ionicLoading.show({
+                template: '<p>Loading...</p><ion-spinner></ion-spinner>',
+            });
+            if(response.success){
+                $ionicLoading.hide();
+                $scope.Order = response.data;
+                initMap($scope.Order.location_ordered.point)
+            }    
+         });      
+    } 
+    getInfor();
+    function initMap(location) {
+        var myDBLoc = {lat : location.latitude, lng : location.longitude}
+        var myLatLng = {lat: -25.363, lng: 131.044};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+            title: 'Hello World!'
+        });
+    }
 })
    
 .controller('loginCtrl',function ($scope, $stateParams,$state, AuthService,$ionicPopup,$ionicLoading,$cordovaOauth,$http) {
@@ -109,7 +182,7 @@ angular.module('app.controllers', [])
             });
             AuthService.register($scope.newUser).then(function(msg){
                 $ionicLoading.hide();
-                $state.go("tabsController.food");  
+                $$state.go("tabsController.orderList");  
             },function(errMsg){
                 $ionicLoading.hide();
                 var alertPopup = $ionicPopup.alert({
